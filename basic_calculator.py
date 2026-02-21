0abe8b070ffc5c57ff7a9226bec594d6caeeb0ca
Anany-Upadhyay <anany.vk09@gmail.com>
2026-02-21 14:56:10 +0530

    Add files via upload

---
+++ basic_calculator.py
@@ -0,0 +1,13 @@
+#basic calculator
+#built while learning python fundamentals
+
+
+num1=float(input("enter the number"))
+num2=float(input("enter the second number"))
+
+print(num1,"+",num2,"=",num1+num2)
+print(num1, "-" ,num2,"=",num1-num2)
+print(num1, "x", num2, "=", num1*num2)
+print(num1, "/", num2, "=", num1/num2)
+
+
